259fe982fd85794616c96a53819ed66a0dcd640d
Liam Galka <gl83991@mail.harper.college>
2025-09-07 19:21:59 -0500

    Assignment Complete

--- app/src/main/java/org/example/Number.java
+++ app/src/main/java/org/example/Number.java
@@ -2,16 +2,16 @@ package org.example;
 
 import java.util.Scanner;
 
-public class MyPassword {
-  public static void askPassword(String prompt, String errorMessage){
+public class Number {
+  public static void guessNumber(String prompt, String errorMessage){
     Scanner input = new Scanner(System.in);
       while (true) {
         System.out.print(prompt);
-        String attempt = input.nextLine();
+        int guess = input.nextInt();
         System.out.println();
       }
     }
   public static void main(String[] args) {
-    askPassword("Enter your password: ", "Incorrect password, try again.");
+    guessNumber("Enter a number: ", "Incorrect, try again.");
   }
 }
